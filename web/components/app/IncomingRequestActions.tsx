a5e72e89a90b7b544471148878f77f45fa6b3a24
nexus bank <ivyjahf1@gmail.com>
2026-09-22 14:30:31 -1100

    Fix Vercel build: add missing use client directive to IncomingRequestActions

--- web/components/app/IncomingRequestActions.tsx
+++ web/components/app/IncomingRequestActions.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState, useTransition } from "react";
 import { Button } from "@/components/ui/Button";
 import { respondRequestAction } from "@/lib/actions/connections";
